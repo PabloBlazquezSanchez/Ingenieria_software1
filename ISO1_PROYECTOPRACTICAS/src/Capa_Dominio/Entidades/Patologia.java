ae6baee380277500688d6c1223e052b7bf049268
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-12-18 16:23:23 +0100

    nn

--- ISO1_PROYECTOPRACTICAS/src/Capa_Dominio/Entidades/Patologia.java
+++
@@ -1,8 +0,0 @@
-package VistaEstatica.Dominio.Entidades;
-
-public class Patologia {
-	private String _nombre;
-	private Date _inicio;
-	private boolean _esCronica;
-	public Historial _historial;
-}
