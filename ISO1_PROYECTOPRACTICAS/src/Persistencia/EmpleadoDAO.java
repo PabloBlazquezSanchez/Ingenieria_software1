c6b4c0d8244047315e59e1ef1111524bdbc03379
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-12-19 21:48:28 +0100

    nn

--- ISO1_PROYECTOPRACTICAS/src/Persistencia/EmpleadoDAO.java
+++ ISO1_PROYECTOPRACTICAS/src/Persistencia/EmpleadoDAO.java
@@ -11,18 +11,26 @@ public class EmpleadoDAO extends UsuarioDAO {
 
 	public static Empleado selectEmpleado(Empleado e) throws SQLException {
 		Vector datosEmpleado = GestorBaseDatos.getInstancia()
-				.select("SELECT * FROM empleado WHERE nombreusario'" + e.get_nombreUsuario() + "'");
+				.select("SELECT * FROM empleado WHERE NOMBREUSUARIO='" + e.get_nombreUsuario() + "'");
 		datosEmpleado = (Vector) datosEmpleado.get(0);
-
+System.out.println(datosEmpleado);
 		if (datosEmpleado.isEmpty()) {
 			System.out.println("Error");
 		} else {
 			String nombre = (String) datosEmpleado.get(1);
 			String apellido = (String) datosEmpleado.get(2);
 			String dni = (String) datosEmpleado.get(0);
-			Rol rol = (Rol) datosEmpleado.get(4);
-			String nombreusuario= (String) datosEmpleado.get(5);
-			String contrasena = (String) datosEmpleado.get(6);
+			String rols=(String) datosEmpleado.get(3);
+			Rol rol=null;
+			for ( Rol d: Rol.values()){
+				 if( d.equals(rols)){
+			            rol = d;
+			        }
+				}
+	
+			
+			String nombreusuario= (String) datosEmpleado.get(4);
+			String contrasena = (String) datosEmpleado.get(5);
 			e = new Empleado(nombre, apellido, dni, nombreusuario, contrasena, 0, rol);
 		}
 		return e;
