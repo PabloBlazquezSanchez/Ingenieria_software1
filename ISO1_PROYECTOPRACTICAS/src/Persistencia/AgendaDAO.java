91c5f1cb473e7c01543c19cbeae6036184a39f17
Jesús <Jesus.Fdez1@alu.uclm.es>
2022-12-21 22:42:23 +0100

    Update AgendaDAO.java
    nn

--- ISO1_PROYECTOPRACTICAS/src/Persistencia/AgendaDAO.java
+++ ISO1_PROYECTOPRACTICAS/src/Persistencia/AgendaDAO.java
@@ -18,14 +18,11 @@ public class AgendaDAO {
 		throw new UnsupportedOperationException();
 	}
 
-	public static int modificarAgenda(String dniesp, String fecha, String id) throws SQLException {
+	public static int modificarAgenda(String dniesp, String fecha, int id) throws SQLException {
 		Vector huecoslibres = GestorBaseDatos.getInstancia().select("SELECT SLOTS FROM agenda WHERE DNIESPECIALISTA='"+dniesp+"'AND DIA='"+fecha+"'");
 		Vector huecoindividual = (Vector) huecoslibres.get(0);
 		String slots = (String) huecoindividual.get(0);
 		String nuevoSlot=slots+", "+id;
-		System.out.println(nuevoSlot);
-		
-		
-		return 0;
+		return GestorBaseDatos.getInstancia().update("UPDATE agenda SET SLOTS='"+nuevoSlot+"' WHERE DNIESPECIALISTA='"+dniesp+"'AND DIA='"+fecha+"'") + 1;
 	}
 }
